590213f08f9f0199525df19efeb14ea083946db8
zan8in <zanbin168@qq.com>
2022-05-07 22:46:54 +0800

    version 1.3.2

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -40,7 +40,7 @@ type Ceye struct {
 }
 
 const afrogConfigFilename = "afrog-config.yaml"
-const Version = "1.3.1"
+const Version = "1.3.2"
 
 // Create and initialize afrog-config.yaml configuration info
 func New() (*Config, error) {
