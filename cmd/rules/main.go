de2405c33a7ffbc4f762689b2969a34704d1ec80
zan8in <zanbin168@qq.com>
2022-06-10 18:10:54 +0800

    added many OA vulner

--- cmd/rules/main.go
+++ cmd/rules/main.go
@@ -4,13 +4,22 @@ import (
 	"fmt"
 
 	"github.com/zan8in/afrog/pkg/catalog"
+	"github.com/zan8in/afrog/pkg/poc"
 )
 
 func main() {
-	c := catalog.New("./pocs/afrog-pocs")
-	allPocsYamlSlice, err := c.GetPocPath("./pocs/afrog-pocs")
+	c := catalog.New("./afrog-pocs")
+	allPocsYamlSlice, err := c.GetPocPath("./afrog-pocs")
 	if err != nil && len(allPocsYamlSlice) == 0 {
 		fmt.Println("未找到可执行脚本(POC)，请检查`默认脚本`或指定新の脚本(POC)")
 	}
 	fmt.Println(len(allPocsYamlSlice))
+	for _, v := range allPocsYamlSlice {
+		poc, err := poc.ReadPocs(v)
+		if err != nil {
+			fmt.Println(v)
+		} else {
+			fmt.Println(poc.Info.Name)
+		}
+	}
 }
