272010093e02aa8237c675b72970fc04473af60d
zan8in <zanbin168@qq.com>
2022-06-14 17:25:15 +0800

    move afrog-pocs to pocs/afrog-pocs

--- cmd/rules/main.go
+++ cmd/rules/main.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	c := catalog.New("./pocs/afrog-pocs")
-	allPocsYamlSlice, err := c.GetPocPath("./pocs/afrog-pocs")
+	c := catalog.New("./afrog-pocs")
+	allPocsYamlSlice, err := c.GetPocPath("./afrog-pocs")
 	if err != nil && len(allPocsYamlSlice) == 0 {
 		fmt.Println("未找到可执行脚本(POC)，请检查`默认脚本`或指定新の脚本(POC)")
 	}
