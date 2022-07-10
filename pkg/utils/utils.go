580556359d5b79f3bfb91798c379b25cd66d06c8
zan8in <zanbin168@qq.com>
2022-07-10 11:45:38 +0800

    add tomcat etc poc

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"encoding/hex"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -171,4 +173,22 @@ func GetNumberText(number int) string {
 		num = "0" + num
 	}
 	return num
+}
+
+//16进制解码
+func HexDecode(s string) []byte {
+	dst := make([]byte, hex.DecodedLen(len(s))) //申请一个切片, 指明大小. 必须使用hex.DecodedLen
+	n, err := hex.Decode(dst, []byte(s))        //进制转换, src->dst
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	return dst[:n] //返回0:n的数据.
+}
+
+//字符串转为16进制
+func HexEncode(s string) []byte {
+	dst := make([]byte, hex.EncodedLen(len(s))) //申请一个切片, 指明大小. 必须使用hex.EncodedLen
+	n := hex.Encode(dst, []byte(s))             //字节流转化成16进制
+	return dst[:n]
 }
