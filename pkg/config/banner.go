c91fe4986449b3e312bdd0aefe76dac858808229
zanbin168 <zanbin168@qq.com>
2023-05-22 16:08:14 +0800

    update

--- pkg/config/banner.go
+++ pkg/config/banner.go
@@ -7,7 +7,7 @@ import (
 	"github.com/zan8in/gologger"
 )
 
-const Version = "2.3.2"
+const Version = "2.5.1"
 
 func ShowBanner(u *upgrade.Upgrade) {
 	gologger.Print().Msgf("\n|\tA F R O G\t>\t%s\t-\t%s\n\n", EngineV(u), PocV(u))
