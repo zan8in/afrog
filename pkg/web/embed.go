2fd16e1a1fb1c7322ec3b27309ddeebfeeca0cbe
zan8in <zanbin168@qq.com>
2025-11-24 10:16:20 +0800

    -web bug  test

--- pkg/web/embed.go
+++ pkg/web/embed.go
@@ -2,25 +2,10 @@ package web
 
 import "embed"
 
-//go:embed webpath/index.html
-//go:embed webpath/login.html
-//go:embed webpath/docs.html
-//go:embed webpath/pocs.html
-//go:embed webpath/reports.html
-//go:embed webpath/favicon.ico
-//go:embed webpath/favicon-16x16.png
-//go:embed webpath/favicon-32x32.png
-//go:embed webpath/android-chrome-192x192.png
-//go:embed webpath/android-chrome-512x512.png
-//go:embed webpath/apple-touch-icon.png
-//go:embed webpath/site.webmanifest
-//go:embed webpath/placeholder.svg
-//go:embed webpath/fonts/Geist/*
-//go:embed webpath/_app/env.js
-//go:embed webpath/_app/immutable/assets/*
-//go:embed webpath/_app/immutable/chunks/*
-//go:embed webpath/_app/immutable/entry/*
-//go:embed webpath/_app/immutable/nodes/*
+//go:embed webpath/*.*
+//go:embed webpath/fonts/*/*.*
+//go:embed webpath/_app/*.*
+//go:embed webpath/_app/immutable/*/*.*
 var webpathFS embed.FS
 
 func GetWebpathFS() embed.FS {
