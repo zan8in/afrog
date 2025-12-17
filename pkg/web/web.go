218104b905fecc54acdbd05860afec53fa7456c3
zan8in <zanbin168@qq.com>
2025-12-17 23:58:21 +0800

    update pocs

--- pkg/web/web.go
+++ pkg/web/web.go
@@ -1,15 +1,15 @@
 package web
 
 import (
-    "net/http"
-    "time"
-    "os"
-    "crypto/rand"
-    "encoding/hex"
+	"crypto/rand"
+	"encoding/hex"
+	"net/http"
+	"os"
+	"time"
 
-    "github.com/zan8in/afrog/v3/pkg/db/sqlite"
-    "github.com/zan8in/gologger"
-    "github.com/zan8in/gologger/levels"
+	"github.com/zan8in/afrog/v3/pkg/db/sqlite"
+	"github.com/zan8in/gologger"
+	"github.com/zan8in/gologger/levels"
 )
 
 var serverInstanceID string
@@ -20,16 +20,16 @@ var serverArgv []string
 var httpSrv *http.Server
 
 func generateInstanceID() string {
-    b := make([]byte, 16)
-    _, _ = rand.Read(b)
-    return hex.EncodeToString(b)
+	b := make([]byte, 16)
+	_, _ = rand.Read(b)
+	return hex.EncodeToString(b)
 }
 
 func StartServer(addr string) error {
-    gologger.DefaultLogger.SetMaxLevel(levels.LevelDebug)
-    generatedPassword = generateRandomPassword()
-    initJWTSecret()
-    gologger.Info().Msgf("Web访问密码: %s", generatedPassword)
+	gologger.DefaultLogger.SetMaxLevel(levels.LevelInfo)
+	generatedPassword = generateRandomPassword()
+	initJWTSecret()
+	gologger.Info().Msgf("Web访问密码: %s", generatedPassword)
 
 	// 初始化数据库（连接 + 写入worker）
 	if err := sqlite.NewWebSqliteDB(); err != nil {
@@ -51,22 +51,22 @@ func StartServer(addr string) error {
 	}
 
 	// 使用 http.Server 并设置超时，提升抗慢速攻击能力
-    srv := &http.Server{
-        Addr:              addr,
-        Handler:           handler,
-        ReadHeaderTimeout: 5 * time.Second,
-        ReadTimeout:       0,
-        WriteTimeout:      0,
-        IdleTimeout:       120 * time.Second,
-    }
+	srv := &http.Server{
+		Addr:              addr,
+		Handler:           handler,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       0,
+		WriteTimeout:      0,
+		IdleTimeout:       120 * time.Second,
+	}
 
-    serverInstanceID = generateInstanceID()
-    serverStartedAt = time.Now().UTC()
-    serverBaseURL = "http://" + addr
-    serverPID = os.Getpid()
-    serverArgv = os.Args
-    httpSrv = srv
+	serverInstanceID = generateInstanceID()
+	serverStartedAt = time.Now().UTC()
+	serverBaseURL = "http://" + addr
+	serverPID = os.Getpid()
+	serverArgv = os.Args
+	httpSrv = srv
 
-    gologger.Info().Msgf("Web服务器启动于: http://%s", addr)
-    return srv.ListenAndServe()
+	gologger.Info().Msgf("Web服务器启动于: http://%s", addr)
+	return srv.ListenAndServe()
 }
